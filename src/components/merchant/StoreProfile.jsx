6141eb473bdd5ce8577c4097fa9fb30b5e124f90
Pranav chandraa <pranavchandraa07@gmail.com>
2026-03-12 09:56:59 +0530

    intial changes is completed

--- src/components/merchant/StoreProfile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StoreProfile = () => {
-  return (
-    <div>StoreProfile</div>
-  )
-}
-
-export default StoreProfile
--- src/components/shopper/FirstCouponPopup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FirstCouponPopup = () => {
-  return (
-    <div>FirstCouponPopup</div>
-  )
-}
-
-export default FirstCouponPopup
