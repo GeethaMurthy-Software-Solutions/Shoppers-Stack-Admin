6a74fcffc69d3d8be1b458d84e27f63afd9d4147
Pranav chandraa <pranavchandraa07@gmail.com>
2026-03-21 08:16:27 +0530

    merchentmanagement layout fixing is done

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from './Navbar'
-import Sidebar from './Sidebar';
-import { Outlet } from 'react-router-dom';
-
+import Sidebar from './Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
-      const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-            <Navbar toggleSidebar={() => setIsOpen(!isOpen)} />
-            
-                <Sidebar isOpen={isOpen} />
-             <div className="mt-10 ml-60 flex-1 overflow-y-auto p-6 bg-gray-50 h-screen">
-                <Outlet />
-            </div>
+      <Navbar />
+
+      <div className="flex">
+        
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <div className="mt-10 flex-1 overflow-y-auto p-6 bg-gray-50 h-screen transition-all duration-300 ml-0 md:ml-60">
+          <Outlet />
+        </div>
+
+      </div>
     </div>
   )
 }
