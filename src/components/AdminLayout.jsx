a1a52e90327552238e1772a9976673ae1df2292c
Pranav chandraa <pranavchandraa07@gmail.com>
2026-03-24 08:41:08 +0530

    adminlayoutres

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
-      <Navbar />
+      <Navbar toggleSidebar={() => setIsOpen(!isOpen)} />
 
       <div className="flex">
         
         {/* Sidebar */}
-        <Sidebar />
+        <Sidebar isOpen={isOpen}  />
 
         {/* Main Content */}
-        <div className="mt-10 flex-1 overflow-y-auto p-6 bg-gray-50 h-screen transition-all duration-300 ml-0 md:ml-60">
+        <div className={`mt-14 flex-1 p-6 transition-all duration-300 
+          ${isOpen ? "ml-60" : "ml-0"} md:ml-60`}>
           <Outlet />
         </div>
 
